0e9239cfcd1697cfa6f76dcb90db036977283d45
iwatchkin <iwatchkin@gmail.com>
2024-02-13 01:43:26 +0300

    rewrote the learning function without cycles

--- agent_ppo.py
+++ agent_ppo.py
@@ -20,7 +20,7 @@ class PPOAgent:
         self.__actor_model_copy = deepcopy(self.__actor_model)
         self.__actor_lr = actor_lr
         self.__critic_lr = critic_lr
-        self.__actor_optimizer = torch.optim.Adam(self.__actor_model_copy.parameters(), lr=actor_lr)
+        self.__actor_optimizer_copy = torch.optim.Adam(self.__actor_model_copy.parameters(), lr=actor_lr)
         self.__critic_optimizer = torch.optim.Adam(self.__critic_model.parameters(), lr=critic_lr)
 
     def get_action(self, state):
@@ -38,6 +38,7 @@ class PPOAgent:
         assert len(self.__trajectories) >= self.__batch_size, \
             "The length of the trajectory must be greater than the size of the mini-batch"
 
+        num_batch = 0
         while True:
             mini_batch = self.__trajectories[:self.__batch_size]
             self.__trajectories = self.__trajectories[self.__batch_size:]
@@ -45,39 +46,27 @@ class PPOAgent:
             if len(mini_batch) == 0:
                 break
 
-            old_policy = []
-            new_policy = []
-            r_t = []
-            a_t = []
-
-            num_batch = 0
-            k = num_batch * self.__batch_size
-            for observation in mini_batch:
-                state, action, reward, _, _ = observation
-                r_t.append(sum(r_t) + self.__gamma ** k * reward)
-                a_t.append(torch.FloatTensor(r_t[-1]) -
-                           self.__critic_model(torch.FloatTensor(list(state) + [action.item()])))
-                old_policy.append(self.__actor_model(torch.FloatTensor(state)))
-                new_policy.append(self.__actor_model_copy(torch.FloatTensor(state)))
-
-                k += 1
-
-            r_t, a_t, old_policy, new_policy = map(torch.FloatTensor, (r_t, a_t, old_policy, new_policy))
-            (r_t.requires_grad_(True), a_t.requires_grad_(True), old_policy.requires_grad_(True),
-             new_policy.requires_grad_(True))
-            states, actions, _, _, _ = map(torch.FloatTensor, zip(*mini_batch))
-            ratio_policy = new_policy / old_policy
-            actor_loss = -torch.mean(torch.min(ratio_policy * a_t,
-                                     torch.clip(ratio_policy, 1 - self.__clip_ratio, 1 + self.__clip_ratio) * a_t))
-
-            self.__actor_optimizer.zero_grad()
-            actor_loss.backward()
-            self.__actor_optimizer.step()
+            states, actions, rewards, _, _ = map(torch.FloatTensor, zip(*mini_batch))
             actions = actions.reshape((self.__batch_size, 1))
-            states_and_actions = torch.cat((states, actions), dim=1)
-            critic_loss = torch.mean((self.__critic_model(states_and_actions).reshape((self.__batch_size, 1)) -
-                                      r_t.reshape((self.__batch_size, 1))) ** 2)
+            rewards = rewards.reshape((self.__batch_size, 1))
 
+            old_policy = self.__actor_model(states)
+            new_policy = self.__actor_model_copy(states)
+            k = num_batch * self.__batch_size
+            discount_rates = (torch.FloatTensor([self.__gamma ** k for k in range(k, k+self.__batch_size)]).
+                              reshape((self.__batch_size, 1)))
+            rewards_to_go = torch.cumsum(discount_rates * rewards, dim=1)
+            states_and_actions = torch.cat((states, actions), dim=1)
+            advantage_estimates = rewards_to_go - self.__critic_model(states_and_actions)
+            ratio_policy = new_policy / old_policy
+
+            actor_loss = -torch.mean(torch.min(ratio_policy * advantage_estimates, torch.clip(ratio_policy, 1 -
+                                                    self.__clip_ratio, 1 + self.__clip_ratio) * advantage_estimates))
+            self.__actor_optimizer_copy.zero_grad()
+            actor_loss.backward()
+            self.__actor_optimizer_copy.step()
+
+            critic_loss = torch.mean((self.__critic_model(states_and_actions) - rewards_to_go) ** 2)
             self.__critic_optimizer.zero_grad()
             critic_loss.backward()
             self.__critic_optimizer.step()
