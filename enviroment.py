944d46534bdc0c4daeb15158b4556c12fba0e80c
iwatchkin <iwatchkin@gmail.com>
2024-02-20 00:40:48 +0300

    new working version

--- enviroment.py
+++ enviroment.py
@@ -1,41 +1,26 @@
 import gym
 import warnings
-from agent_ddpg import DDPGAgent
 from agent_ppo import PPOAgent
 from plot_history_rewards import plot_history_rewards
 import os
+
+
+class Environment:
+    def __init__(self, env):
+        self.env = env
+
+    def get_initial_state(self):
+        return self.env.reset()[0]
+
+    def step_in_environment(self, action):
+        return self.env.step(action)
 
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 warnings.filterwarnings("ignore")
 
 env = gym.make('Pendulum-v1', g=9.81)
-#episodes, trajectory_len = 100, 200
-
-#agent = DDPGAgent(state_dim=3, action_dim=1)
-#history_rewards_ddpg = []
-
-# print("DDPG algorithm\n")
-# for episode in range(episodes):
-#
-#     total_reward = 0
-#     state = env.reset()[0]
-#     for _ in range(trajectory_len):
-#         action = agent.get_action(state)
-#         next_state, reward, done, _, _ = env.step(action)
-#         total_reward += reward
-#
-#         agent.fit(state, action, reward, done, next_state)
-#
-#         if done:
-#             break
-#
-#         state = next_state
-#     history_rewards_ddpg.append(total_reward / trajectory_len)
-#     print(f"\nEpisode {episode + 1}, total reward {total_reward / trajectory_len}")
-#
-# print(f"\nBest reward: {max(history_rewards_ddpg)}")
-# print(f"Average reward: {sum(history_rewards_ddpg) / len(history_rewards_ddpg)}")
+environment = Environment(env)
 
 episodes, batch_size, num_batch = 100, 200, 10
 trajectory_len = batch_size * num_batch
@@ -44,15 +29,14 @@ agent = PPOAgent(state_dim=3, action_dim=1, actor_lr=1e-4, critic_lr=1e-3, gamma
                  clip_ratio=0.2, action_scale=2, batch_size=batch_size)
 history_rewards_ppo = []
 
-
 print("\nPPO algorithm")
 for episode in range(episodes):
 
     total_reward = 0
-    state = env.reset()[0]
+    state = environment.get_initial_state()
     for _ in range(trajectory_len):
         action = agent.get_action(state)
-        next_state, reward, done, _, _ = env.step(action)
+        next_state, reward, done, _, _ = environment.step_in_environment(action)
         total_reward += reward
         agent.collect_trajectories(state, action, reward, done, next_state)
 
@@ -68,4 +52,7 @@ for episode in range(episodes):
 
 print(f"\nBest reward: {max(history_rewards_ppo)}")
 print(f"Average reward: {sum(history_rewards_ppo) / len(history_rewards_ppo)}")
-plot_history_rewards(history_rewards_ppo)
+plot_history_rewards(history_rewards_ppo, n=3, label="PPO")
+
+
+
--- plot_history_rewards.py
+++ plot_history_rewards.py
@@ -1,9 +1,14 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
-def plot_history_rewards(history_rewards):
-    plt.plot(history_rewards)
-    plt.xlabel('episode')
-    plt.ylabel('reward')
+def plot_history_rewards(history_rewards, n=3, label=None):
+    moving_average_reward = pd.Series(history_rewards).rolling(window=n).mean().iloc[n - 1:].values
+    plt.plot(moving_average_reward, label=label)
+    plt.xlabel('Episode')
+    plt.ylabel('Moving average of the reward')
     plt.title('History of learning')
+    plt.legend()
     plt.show()
+
+
